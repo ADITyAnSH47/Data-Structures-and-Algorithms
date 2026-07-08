2d051239ade24a80c91903c51233671f4d44ba39
ADITyAnSH47 <adityansh05@gmail.com>
2026-07-08 08:19:54 +0530

    O(n) soln , if val exist return root else return NULL

---
+++ Tree/Tree/LC_0098_Validate_binary_Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void inorder(vector<int> &ans,TreeNode* root){
+if(root==NULL){
+    return;
+}
+inorder(ans,root->left);
+ans.push_back(root->val);
+inorder(ans,root->right);
+
+}
+    bool isValidBST(TreeNode* root) {
+        vector<int> ans;
+        inorder(ans,root);
+        for(int i=1;i<ans.size();i++){
+            if(ans[i-1]>=ans[i]){
+                return false;
+            }
+        }       
+        return true; 
+    }
+};
---
+++ Tree/Tree/LC_0700_Search.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        // if(root==NULL){
+        //     return NULL;
+        // }
+        while(root){
+        if(root->val==val){
+            return root;
+        }
+        if(val>root->val && root->right!=NULL){
+            root=root->right;
+             }else if(val>root->val && root->right==NULL){
+                return NULL;
+             }
+        if(val<root->val && root->left!=NULL){
+            root=root->left;
+        }else if(val<root->val && root->left==NULL){
+                return NULL;
+             }
+        }
+        // if(root->left){
+        //     root=root->left;       
+        // }
+         return NULL;
+
+    }
+};
